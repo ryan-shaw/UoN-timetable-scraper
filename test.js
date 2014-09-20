e6726ca2ecfeabbb8e6d5279957227ba37d2e06f
ryan <ryan.shaw@min.vc>
2014-09-20 17:43:01 +0100

    Removed test

--- test.js
+++ test.js
@@ -1,23 +1,23 @@
 var API = require('./api.js');
 require('dotenv').load();
 
-exports['check course scraper'] = function(test){
-	var course = API.CourseScraper().init('0003193');
-	test.expect(1);
-	course.then(function(data){
-		var count = 0;
-		var i;
-		for (i in data) {
-		    if (data.hasOwnProperty(i)) {
-		        count++;
-		    }
-		}
-		test.ok(count === 5, '5 days');		
-		test.done();
-    }, function(err){
-    	console.log(err);
-    });
-};
+// exports['check course scraper'] = function(test){
+// 	var course = API.CourseScraper().init('0003193');
+// 	test.expect(1);
+// 	course.then(function(data){
+// 		var count = 0;
+// 		var i;
+// 		for (i in data) {
+// 		    if (data.hasOwnProperty(i)) {
+// 		        count++;
+// 		    }
+// 		}
+// 		test.ok(count === 5, '5 days');		
+// 		test.done();
+//     }, function(err){
+//     	console.log(err);
+//     });
+// };
 
 exports.tearDown = function(done){
 	API.close(done);
