8fd89d83e1144eac1a9376292ca190d793bf2ddc
Ryan Shaw <ryan.shaw@min.vc>
2015-10-05 22:54:43 +0100

    fix(api): Fix for API not removing duplicate students on cache refresh
    Attempt to remove the student by username if non found in past day.

--- api.js
+++ api.js
@@ -345,6 +345,7 @@ exports.getCourseByUsername = function(username, callback){
     username = username.toLowerCase();
     StudentModel.findOne({username: username, time_stamp: {$gte: (Date.now() - (1000 * 60 * 60 * 24))} }, function(err, student){
         if(!student){
+            StudentModel.remove({username: username});
             getJson('https://ws.nottingham.ac.uk/person-search/v1.0/student/'+username, function(err, studentData){
                 if(err || studentData.results.length === 0) return callback(null);
                 exports.getCourseByName(studentData.results[0]._courseName, studentData.results[0]._yearOfStudy, function(data){
